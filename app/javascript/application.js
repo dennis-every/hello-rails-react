6cfb76bca30f801aaf90d06eeb6ee5ca3b60684c
Dennis Every <dennis@ansyra.com>
2023-06-06 18:08:52 -0400

    Add react-router-dom

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Entry point for the build script in your package.json
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return <h1>Hello World!</h1>;
@@ -8,5 +9,7 @@ function App() {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
